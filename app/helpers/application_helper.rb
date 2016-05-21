2c8c57cef3c14f845014e8231db1e4d6758207f6
Pablo Sanabria <psq1@ucbcba.edu.bo>
2016-05-21 11:51:23 -0400

    Cosas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def get_title(title)
+    "#{title} | UCBTweet"
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,6 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "title", "Home | UCBTweet"
   end
 
 end
